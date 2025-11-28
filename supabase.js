8b0e4555e15c0ea0729fe0f0a20975f219d589ee
Ansteikoku <tianzhongxiaoji75@gmail.com>
2025-11-29 00:49:48 +0900

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
 // Supabase 設定
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://egoxcxdwsgjdlnrjbnec.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnb3hjeGR3c2dqZGxucmpibmVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMzUxMDQsImV4cCI6MjA3OTkxMTEwNH0.tr3Bvd6tQEGSog6xjJ8mdi7vFzPBsrhIbzA5-M8OBic";
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
